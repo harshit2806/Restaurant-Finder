e8694cfeda5c8de84fd6c63a0d3dc1afdca27463
Harshit <harshitsoni2806@gmail.com>
2022-01-08 16:19:09 +0530

    test

--- Android-Restaurant-Finder-master/app/src/test/java/com/example/resfind/ExampleUnitTest.java
+++ Android-Restaurant-Finder-master/app/src/test/java/com/example/resfind/ExampleUnitTest.java
@@ -4,14 +4,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+	public void substraction_isCorrect() {
+        assertEquals(4, 6 - 2);
+    }
+	public void multiplication_isCorrect() {
+        assertEquals(12, 6 * 2);
+    }
+	public void divide_isCorrect() {
+        assertEquals(3, 6 / 2);
     }
 }
